a7784e54a72fbc564016bfb03930ed0feca6f399
Sandip Das <codersandipdas@gmail.com>
2024-08-25 11:29:50 +0530

    used otp inout on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,32 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import OtpInput from '$lib/OtpInput.svelte';
+
+  let otpValue = '';
+
+  function handleOtpChange(newOtpValue: string) {
+    otpValue = newOtpValue;
+  }
+</script>
+
+<section
+  class="h-screen flex flex-col gap-4 justify-center w-[400px] max-w-full mx-auto"
+>
+  <p class="text-left text-xl font-semibold text-black">Enter OTP</p>
+  <p class="text-left text-base font-normal -mt-3 mb-2 text-black/50">
+    Type or paste the OTP sent to you
+  </p>
+  <OtpInput
+    length={6}
+    inputType="number"
+    placeholder="•"
+    value={otpValue}
+    onChange={handleOtpChange}
+    inputClass="py-1 w-[40px]"
+    containerClass="w-full overflow-hidden w-[400px]"
+  />
+  <div>
+    <button class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-1.5 rounded"
+      >Verify OTP</button
+    >
+  </div>
+</section>
